db78e15fdd27f7b2cbc04f923b8ad97ae9b34cf8
CanadianBias <157527162+CanadianBias@users.noreply.github.com>
2024-02-28 09:46:23 -0600

    Created file with code from netID.sh

---
+++ subnetCalc.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# Calculates possible subnet configurations based 
+
+function convertNetmask {
+#Convert slash notation to netmask
+#Assumes slash is non-zero
+#Convert slash notation to netmask
+        for ((i=0;i<4;i++)); do
+                if [[ slash -ge 8 ]]; then
+                        netmask[$i]=255
+                        slash=$[slash-8]
+                else
+                        case $slash in 
+                                0 ) netmask[i]=0;;
+                                1 ) netmask[i]=128;;
+                                2 ) netmask[i]=192;;
+                                3 ) netmask[i]=224;;
+                                4 ) netmask[i]=240;;
+                                5 ) netmask[i]=248;;
+                                6 ) netmask[i]=252;;
+                                7 ) netmask[i]=254;;
+                        esac
+			slash=0
+                fi
+	done
+}
+
+# Override IFS to seperate addresses on dots
+oldIFS=$IFS
+
+# Check for CLAs and populate IP and NetMask
+if [[ $# -eq 0 ]]; then
+	IFS="."
+	echo -n "Enter your IP address: "
+	read -a IP
+	echo -n "Enter your Netmask: "
+	read netmaskInput
+	case $netmaskInput in
+                255.*.*.* ) read -a netmask <<< $netmaskInput;;
+                /* ) slash=${2:1}
+                        #Convert slash notation to netmask
+                        convertNetmask;;
+                [0-9] | [0-3][0-9] | 30 ) slash=$netmaskInput
+                        #Convert slash notation to netmask
+                        convertNetmask;;
+                [aAbBcC] ) case $netmaskInput in
+                        [aA] ) slash=8;;
+                        [bB] ) slash=16;;
+                        [cC] ) slash=24;;
+                        esac
+                        convertNetmask;;
+        esac
+	IFS=$oldIFS
+# The elif below needs error checking:
+	# Did they give me an IP, netmask, IP/bits?
+	# What do I do with what they gave me
+elif [[ $# -eq 1 ]]; then
+	case $1 in
+		*.*.*.*/* ) IFS="./"
+			read -a IP <<< $1 # gives five element array
+			IFS=$oldIFS
+			slash=${IP[4]}
+			convertNetmask;;
+		255.*.*.* ) read -a netmask <<< $1
+			echo -n "Enter your IP address: "
+			read -a IP;;
+		*.*.*.* ) read -a IP <<< $1
+			echo -n "Enter your Netmask: "
+      			read netmaskInput
+       			case $netmaskInput in
+                		255.*.*.* ) read -a netmask <<< $netmaskInput;;
+                		/* ) slash=${2:1}
+                        		#Convert slash notation to netmask
+                	        	convertNetmask;;
+         	      	 	[0-9] | [0-3][0-9] | 30 ) slash=$netmaskInput
+                       		 	#Convert slash notation to netmask
+                       		 	convertNetmask;;
+               		 	[aAbBcC] ) case $netmaskInput in
+                	        	[aA] ) slash=8;;
+	                        	[bB] ) slash=16;;
+        	                	[cC] ) slash=24;;
+                	        	esac
+				        convertNetmask;;
+			esac
+	esac
+	IFS=$oldIFS
+elif [[ $# -eq 2 ]]; then
+	IFS="."
+	read -a IP <<< $1 # <<< redirects as input
+	#read -a netmask <<< $2
+	# Dr. Elliot's in-class example (that's probably better than mine)
+		case $2 in
+		255.*.*.* ) read -a netmask <<< $2;;
+		/* ) slash=${2:1}
+			#Convert slash notation to netmask
+			convertNetmask;;
+		[0-9] | [0-3][0-9] | 30 ) slash=$2
+			#Convert slash notation to netmask
+			convertNetmask;;
+		[aAbBcC] ) case $2 in
+			[aA] ) slash=8;;
+			[bB] ) slash=16;;
+			[cC] ) slash=24;;
+			esac
+			convertNetmask;;
+	esac
+	IFS=$oldIFS
+fi # should now have two arrays with the info
+
+# Calculate NetID from IP and netmask
+for ((i=0;i<4;i++)); do
+	octet=${IP[i]}
+	mask=${netmask[i]}
+	network[i]=$[octet & mask] #bitwise AND
+	#if [[ $mask -eq 255 ]]; then
+	#	slash=$[slash+8]
+	#else
+
+	#fi
+	case $mask in
+		255 ) slash=$[slash+8];;
+		254 ) slash=$[slash+7];;
+		252 ) slash=$[slash+6];;
+		248 ) slash=$[slash+5];;
+		240 ) slash=$[slash+4];;
+		224 ) slash=$[slash+3];;
+		192 ) slash=$[slash+2];;
+		128 ) slash=$[slash+1];;
+		0 ) slash=$[slash];;
+		* ) echo "The  subnet mask was entered incorrectly. Please reload the program."
+			exit;;
+	esac
+done
+
+echo "The Network ID of $1 is ${network[0]}.${network[1]}.${network[2]}.${network[3]}/$slash"
