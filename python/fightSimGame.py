367f3abd2958ae8c5d38e2646e2a4bd3c1fad070
Elijah Dromarsky <157527162+CanadianBias@users.noreply.github.com>
2024-04-22 10:12:45 -0500

    Moved inventory display all to same function, added combat comments, split items and resource classes

--- python/fightSimGame.py
+++ python/fightSimGame.py
@@ -6,12 +6,16 @@
 
 import random as rand
 import os
+# Testing references to user-created libraries
+# import myGameChar as ch
 
 class Player():
     def __init__(self, name):
         self.name = name
         self.currenthp = rand.randint(8,15)
         self.maxhp = self.currenthp
+        self.currentap = rand.randint(4,10)
+        self.maxap = self.currentap
         self.alive = True # changed if currenthp drops below 1
         self.strength = rand.randint(1,10) # determines flat damage output
         self.perception = rand.randint(1,10) # increases likeliness to find items
@@ -36,17 +40,28 @@ class Player():
         print("Luck: " + str(self.luck))
     def displayInventory(self):
         # os.system('clear')
-        for i in self.inventory:
-            print(i.name)
+        # Placeholder space for a random enemy encounter when you open your inventory
+        print(addFancyThings("Inventory", "-"))
+        for i,item in enumerate(self.inventory):
+                print(str(i) + ": " + str(item.name))
+        print(addFancyThings("", "-"))
+        print("1: Select an Item or Resource")
+        print("2: Exit Inventory")
+        inventorySelection = checkUserInput()
+        if inventorySelection == 1:
+            print(str(self.inventory[0].name))
+        if inventorySelection == 2:
+            return
     def equipItem(self):
         # Take equippable item, change equip status, increment max health/strength
         pass
     def unequipItem(self):
         # Take equipped item, change equip status, deincrement max health/strength
         pass
-    def findItem(self):
+    def findResource(self):
+        # Placeholder for chance of enemy spawning when looking for an item
         # Check perception stat, adjust probability needed to find item accordingly
-        # Create random stat using lists of possible items,
+        # Create random stat using lists of possible items
         pass
     def craftItem(self):
         # Give player list of items able to be crafted and resources required
@@ -67,17 +82,39 @@ class Game():
     def walkForward(self):
         self.distance += 100
 
-class Item():
-    def __init__(self, name) -> None:
-        # Name of item
-        self.name = name
-        # Definition determining type of item
-        # If debuff items, have respective stats or placeholder for different types
-        # Crafting class if item is a crafting item
-        # Health/strength stat change if item is armor/weapon
-        # Equipped status if equipped
-        pass
+# Defining resources and items in the same class was too cumbersome, moving to seperate definitions
+# class Item():
+#     def __init__(self, name, boolResource, strStat, armorStat, boolWood, boolStone) -> None:
+#         # Name of item
+#         self.name = name
+#         self.isResource = boolResource # boolean to determine whether it is a resource or item
+#         # If debuff items, have respective stats or placeholder for different types
+#         # These values will be zeroes for resources
+#         self.strengthDebuff = strStat
+#         self.armorDebuff = armorStat
+#         # Crafting class if item is a crafting item
+#         # These will be false for equipment
+#         self.isWood = boolWood
+#         self.isStone = boolStone
+#         # Equipped status if equipped
+#         self.isEquipped = False # false by default as the player will need to manually equip the item
+#         pass
 
+class Item():
+    def __init__(self, name, apCost):
+        self.name = name
+        # Bools to let inventory know difference between resource and item
+        self.isItem = True
+        self.isResource = False
+        self.apCost = apCost
+
+class Resource():
+    def __init__(self, name):
+        self.name = name
+        # Bools to let inventory know difference between resource and item
+        self.isItem = False
+        self.isResource = True
+        
 def addFancyThings(string, char): # Adds some title flair
     stopPoint = os.get_terminal_size()[0]/2
     i = 0
@@ -92,6 +129,16 @@ def addFancyThings(string, char): # Adds some title flair
         j += 1
     return line
 
+# Simple function to check user input so that way I don't need to type out this try-except a million times
+def checkUserInput():
+    while True:
+        try:
+            selection = int(input("Enter a number: "))
+            break
+        except ValueError:
+            print("Selection not understood, please try again.")
+    return selection
+
 
 def coreGame(me, game):
     while True:
@@ -102,23 +149,40 @@ def coreGame(me, game):
         print("Distance Travelled: " + str(game.distance) + "m")
         print(addFancyThings("", "-"))
         # Give player option to move forward, check inventory, look around for items, or craft items
-        print("1: Open Invetory Menu")
+        print("1: Open Inventory Menu")
         print("2: Open Crafting Menu")
         print("3: Look for Items")
         print("4: Continue Pressing Onward")
-        selection = input()
-        if int(selection) == 1:
-            newItem = Item("Sword")
-            me.inventory.append(newItem)
-            print(addFancyThings("Inventory", "-"))
+        print("5: End Your Journey")
+        actionSelection = checkUserInput() # reference to input function
+        if actionSelection == 1:
+            # newItem = Item("Sword")
+            # me.inventory.append(newItem)
             me.displayInventory()
-            print(addFancyThings("", "-"))
+                
+                    
+
+        # elif actionSelection == 2:
+        # elif actionSelection == 3:
+        # elif actionSelection == 4:
+        # elif actionSelection == 5:
         # When looking around or moving forward, higher probability of encountering enemy
         # When looking in inventory or crafting, lower probability of encountering enemy
 
     pass
 
 def combat(me):
+    # Generate new Enemy instance with random name
+    # While loop that is broken when either enemy or player is dead
+    # Clear screen and print enemy information and actions available
+    # If choosing to attack
+        # Check to see if player has enough action points to attack with current weapon
+        # Roll to see if either attack is dodged or if player/enemy missed
+        # Combat damage is dealt
+        # Check to see if enemy or player is dead
+        # Enter next turn
+    # If choosing to open inventory
+        # 
     pass
 
 def main():
